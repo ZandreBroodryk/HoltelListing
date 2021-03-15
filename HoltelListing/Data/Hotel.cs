297834a48f373c9d08a211c540b8a549a0162e67
Zandré Broodryk <Z.Broodryk@gmail.com>
2021-03-15 19:46:26 +0200

    Video 10.2 database created and migrated

--- HoltelListing/Data/Hotel.cs
+++ HoltelListing/Data/Hotel.cs
@@ -13,7 +13,8 @@ namespace HoltelListing.Data
         public string Address { get; set; }
         public double Rating { get; set; }
         [ForeignKey(nameof(Country))]
-        public Country Country { get; set; }
         public int CountryId { get; set; }
+        public Country Country { get; set; }
+        
     }
 }
