88e648e1da2e0b204db62b30e5273ca1679a801e
Zandré Broodryk <Z.Broodryk@gmail.com>
2021-03-17 19:35:45 +0200

    Video 11 seeded data into database

--- HoltelListing/Data/DatabaseContext.cs
+++ HoltelListing/Data/DatabaseContext.cs
@@ -10,7 +10,56 @@ namespace HoltelListing.Data
     {
         public DatabaseContext(DbContextOptions options) : base(options)
         { }
-        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Jamaica",
+                    ShortName = "JM"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Bahamas",
+                    ShortName = "BS"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Cayman Islands",
+                    ShortName = "CI"
+                }
+                );
+
+            builder.Entity<Hotel>().HasData(
+               new Hotel
+               {
+                   ID = 1,
+                   Name = "Sandals Resort and Spa",
+                   Address = "Negril",
+                   CountryId = 1,
+                   Rating = 4
+               },
+               new Hotel
+               {
+                   ID = 2,
+                   Name = "Grand Palladium",
+                   Address = "Nassua",
+                   CountryId = 2,
+                   Rating = 3
+               },
+               new Hotel
+               {
+                   ID = 3,
+                   Name = "Comfort Suites",
+                   Address = "Georgetown",
+                   CountryId = 3,
+                   Rating = 4
+               }
+               );
+        }
         public DbSet<Country> Countries { get; set; }
 
         public DbSet<Hotel> Hotels { get; set; }
--- HoltelListing/Migrations/20210317173155_SeedingData.Designer.cs
+++ HoltelListing/Migrations/20210317173155_SeedingData.Designer.cs
@@ -3,14 +3,16 @@ using HoltelListing.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HoltelListing.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20210317173155_SeedingData")]
+    partial class SeedingData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
