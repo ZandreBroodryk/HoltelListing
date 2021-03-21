703dc54b4fb00276a721185529b41fbf71f8d558
Zandré Broodryk <Z.Broodryk@gmail.com>
2021-03-21 12:10:44 +0200

    VIdeo 12 implimenting CRUD syntax

---
+++ HoltelListing/Repository/GenericRepository.cs
@@ -0,0 +1,90 @@
+﻿using HoltelListing.Data;
+using HoltelListing.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HoltelListing.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DatabaseContext _context;
+        private readonly DbSet<T> _db;
+
+        public GenericRepository(DatabaseContext context)
+        {
+            _context = context;
+            _db = _context.Set<T>();
+        }
+        public async Task Delete(int id)
+        {
+            var entity = await _db.FindAsync(id);
+                _db.Remove(entity);
+            //throw new NotImplementedException();
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _db.RemoveRange(entities);
+            //throw new NotImplementedException();
+        }
+
+        public async Task<T> Get(Expression<Func<T, bool>> expression, List<string> includes = null)
+        {
+            IQueryable<T> query = _db;
+            if (includes != null)
+            {
+                foreach (var includeProperty in includes)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            return await query.AsNoTracking().FirstOrDefaultAsync(expression);
+           // throw new NotImplementedException();
+        }
+
+        public async Task<IList<T>> GetAll(Expression<Func<T, bool>> expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string> includes = null)
+        {
+            IQueryable<T> query = _db;
+            if (expression != null)
+            {
+                query = query.Where(expression);
+            }
+            if (includes != null)
+            {
+                foreach (var includeProperty in includes)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+            return await query.AsNoTracking().ToListAsync();
+            //throw new NotImplementedException();
+        }
+
+        public async Task InserRange(IEnumerable<T> entities)
+        {
+            await _db.AddRangeAsync(entities);
+            //throw new NotImplementedException();
+        }
+
+        public async Task Insert(T entity)
+        {
+            await _db.AddAsync(entity);
+            //throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            _db.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            //throw new NotImplementedException();
+        }
+    }
+}
