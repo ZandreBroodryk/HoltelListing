ae130ad3db86e0eb43670e8fbf79d34f3ad0e9e9
Zandré Broodryk <Z.Broodryk@gmail.com>
2021-03-22 20:15:20 +0200

    Added the usercontroller and added teh code to creat a user.

--- HoltelListing/Configurations/MapperInitializer.cs
+++ HoltelListing/Configurations/MapperInitializer.cs
@@ -16,6 +16,7 @@ namespace HoltelListing.Configurations
             CreateMap<Country, CreateCountryDTO>().ReverseMap();
             CreateMap<Hotel, HotelDTO>().ReverseMap();
             CreateMap<Hotel, CreateHotelDTO>().ReverseMap();
+            CreateMap<ApiUser, UserDTO>().ReverseMap();
         }
     }
 }
